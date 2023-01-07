b409690791d4c88f3c166c511df4c04c7570860b
rifqi <rifqi.rizaldi@jsm.co.id>
2023-01-07 14:44:35 +0700

    fix/remove fixed-bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Row, Col } from "react-bootstrap";
 const Footer = () => {
   return (
     <div>
-      <footer className='navbar-child fixed-bottom navBg'>
+      <footer className='navbar-child navBg'>
         <Row>
           <Col className="text-center py-3 text-white">Copyright &copy; MusicKito</Col>
         </Row>
